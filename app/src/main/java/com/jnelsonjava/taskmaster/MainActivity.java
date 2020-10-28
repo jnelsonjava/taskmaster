915cd682c6aed6df7fd86331748ba9990c021d6e
Jack Nelson <jnelson.java@gmail.com>
2020-10-28 10:50:20 -0700

    subscription and handler added

--- app/src/main/java/com/jnelsonjava/taskmaster/MainActivity.java
+++ app/src/main/java/com/jnelsonjava/taskmaster/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jnelsonjava.taskmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,15 +9,21 @@ import androidx.room.Room;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.ApiOperation;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.api.graphql.model.ModelSubscription;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.TaskInstance;
 
@@ -26,12 +33,12 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskListenable {
 
     Database db;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // initialize amplify
         try {
@@ -43,30 +50,33 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskL
             Log.e("MainActivityAmplify", "Could not initialize Amplify", error);
         }
 
-
-
-
-
-
-
-
-        db = Room.databaseBuilder(getApplicationContext(), Database.class, "jnelson_task_master")
-                .allowMainThreadQueries()
-                .build();
-
-        List<TaskInstance> tasks = db.taskInstanceDAO().getTasksSortByRecent();
-
-//        Amplify.API.query(
-//                ModelQuery.list(TaskInstance.class)),
-//                response -> {
-//                    for (TaskInstance task : response.getData()) {
-//                        Log.i("MainActivityAmplify", task.getTitle());
-//                    }
-//                },
-//                error -> Log.e("MainActivityAmplify", "Query failure", error)
-//        );
-
         List<TaskInstance> taskInstances = new ArrayList<>();
+
+
+        Handler handler = new Handler(Looper.getMainLooper(),
+                new Handler.Callback() {
+                    @Override
+                    public boolean handleMessage(@NonNull Message msg) {
+                        recyclerView.getAdapter().notifyDataSetChanged();
+                        return false;
+                    }
+                });
+
+        Handler handleItemAdded = new Handler(Looper.getMainLooper(),
+                message -> {
+                    recyclerView.getAdapter().notifyItemInserted(taskInstances.size() - 1);
+                    Toast.makeText(this, "New Task!", Toast.LENGTH_SHORT).show();
+                    return false;
+                });
+
+
+
+//        db = Room.databaseBuilder(getApplicationContext(), Database.class, "jnelson_task_master")
+//                .allowMainThreadQueries()
+//                .build();
+//
+//        List<TaskInstance> tasks = db.taskInstanceDAO().getTasksSortByRecent();
+
 
         Amplify.API.query(
                 ModelQuery.list(TaskInstance.class),
@@ -74,16 +84,33 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskL
                     for (TaskInstance taskInstance : response.getData()) {
                         taskInstances.add(taskInstance);
                     }
-//                    handler.sendEmptyMessage(1);
+                    handler.sendEmptyMessage(1);
                     Log.i("Amplify.queryitems", "Got this many items from dynamo " + taskInstances.size());
 
 
                 },
                 error -> Log.i("Amplify.queryitems", "Did not get items"));
 
-        RecyclerView recyclerView = findViewById(R.id.task_list_main_recycler_view);
+
+
+        recyclerView = findViewById(R.id.task_list_main_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new TaskAdapter(taskInstances, this));
+
+
+
+        ApiOperation subscription = Amplify.API.subscribe(
+                ModelSubscription.onCreate(TaskInstance.class),
+                onEstablished -> Log.i("Amplify.Subscription", "Subscription established"),
+                onCreated -> {
+                    Log.i("Amplify.Subscription", "Todo create subscription received: " + ((TaskInstance) onCreated.getData()).getTitle());
+                    TaskInstance newTask = (TaskInstance) onCreated.getData();
+                    taskInstances.add(newTask);
+                    recyclerView.getAdapter().notifyItemInserted(1);
+                },
+                onFailure -> Log.e("Amplify.Subscription", "Subscription failed", onFailure),
+                () -> Log.i("Amplify.Subscription", "Subscription completed")
+        );
 
 
         Button addTaskButton = MainActivity.this.findViewById(R.id.add_task_button);
@@ -124,10 +151,10 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.TaskL
         String updatedText = preferences.getString("username", "") + " tasks";
         taskListTitle.setText(updatedText);
 
-        db = Room.databaseBuilder(getApplicationContext(), Database.class, "jnelson_task_master")
-                .allowMainThreadQueries()
-                .build();
-        List<TaskInstance> tasks = db.taskInstanceDAO().getTasksSortByRecent();
+//        db = Room.databaseBuilder(getApplicationContext(), Database.class, "jnelson_task_master")
+//                .allowMainThreadQueries()
+//                .build();
+//        List<TaskInstance> tasks = db.taskInstanceDAO().getTasksSortByRecent();
 
 
 
